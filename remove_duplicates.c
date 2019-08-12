c6ac71453b606f555b96ec31a419fb13dc29685f
shivika24 <43493690+shivika24@users.noreply.github.com>
2019-08-12 13:25:44 +0530

    remove_duplicates

--- remove_duplicates.c
+++ remove_duplicates.c
@@ -1,22 +1,50 @@
 #include<stdio.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void push(struct Node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+    /* put in the data  */
+    new_node->data  = new_data;
+
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+
+    /* move the head to point to the new node (head is changed as we are adding the node in the beggining)*/
+    (*head_ref) = new_node;
+}
+void printList(struct Node* n)
+{
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
 int main()
 {
-    int n,k=0;
-    printf("enter the size of array=");
-    scanf("%d",&n);
-    int arr[n],i;
-    for(i=0;i<n;i++)
-        scanf("%d",&arr[i]);
-    for(i=0;i<n;i++)
+    struct Node* head = NULL;
+    push(&head, 1);
+    push(&head, 3);
+    push(&head, 3);
+    push(&head, 8);
+    push(&head, 8);
+    //struct Node* curr = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* curr = head;
+    while(curr&&curr->next)
     {
-        if(arr[i]==arr[i+1])
-        arr[i]=0;
-        if(arr[i]!=0)
+        if(curr->data == curr->next->data)
         {
-            arr[k]=arr[i];
-            k++;
+        curr->next=curr->next->next;
         }
+        else
+        curr=curr->next;
     }
-    for(i=0;i<k;i++)
-        printf("%d ",arr[i]);
+    printList(head);
+
+
 }
