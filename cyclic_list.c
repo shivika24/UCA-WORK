481f21f1d9b8ad0d5c250f9fc9ca6ef828c107ee
shivika24 <43493690+shivika24@users.noreply.github.com>
2019-08-12 13:33:14 +0530

    to determine linked list is cyclic or not

---
+++ cyclic_list.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void push(struct Node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+    /* put in the data  */
+    new_node->data  = new_data;
+
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+
+    /* move the head to point to the new node (head is changed as we are adding the node in the beggining)*/
+    (*head_ref) = new_node;
+}
+int main()
+{
+    struct Node* head = NULL;
+
+    /* Use push() to construct below list
+    1->2->1->3->1 */
+    push(&head, 1);
+    push(&head, 3);
+    push(&head, 9);
+    push(&head, 2);
+    push(&head, 8);
+    //struct Node* curr = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* p1 = head;
+    //struct Node* p2 = NULL;
+    struct Node* curr = head;
+    while(p1!=NULL&&p1->next!=NULL)
+    {
+        p1=p1->next->next;
+        curr=curr->next;
+        if(p1==curr)
+        {
+            printf("cyclic");
+            break;
+        }
+    }
+    printf("non cyclic");
+    //printf("%d",curr->data);
+
+
+}
